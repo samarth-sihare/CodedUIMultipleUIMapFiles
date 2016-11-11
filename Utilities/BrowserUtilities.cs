e5be20710bef7301bd658403ee5a9cfdfa1b377d
Samarth <samarth.sihare@yahoo.in>
2016-11-11 12:43:16 +0530

    adding few methods for HTML Table

--- Utilities/BrowserUtilities.cs
+++ Utilities/BrowserUtilities.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
 using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
 using System;
 using System.Collections.Generic;
@@ -55,6 +56,36 @@ namespace CodedUIMultipleUIMapFiles
             Mouse.Click(close);
         }
 
+        ///<summary>
+        ///Few methods for HTML Table
+        ///</summary>
+        private HtmlCell cellByRowColumnIndex(int rowIndex, int columnIndex, HtmlTable table)
+        {
+            HtmlCell cell = new HtmlCell(table);
+            cell.TechnologyName = "Web";
+            cell.SearchProperties.Add(HtmlCell.PropertyNames.RowIndex, rowIndex.ToString());
+            cell.SearchProperties.Add(HtmlCell.PropertyNames.ColumnIndex, columnIndex.ToString());
+
+            return cell;
+        }
+
+        private HtmlRow getRowContaining(string text, HtmlTable table)
+        {
+            HtmlRow row = new HtmlRow(table);
+            row.SearchProperties.Add(HtmlRow.PropertyNames.InnerText, text, PropertyExpressionOperator.Contains);
+            return row;
+        }
+
+        //This method can check a checkbox in a table that has certain string in its row
+        //This method may be modified to get Table parameter accordingly
+        public void checkCheckBoxWithStringInRow(string companyName, HtmlTable table)
+        {
+            HtmlCheckBox countryChkBox = new HtmlCheckBox(this.getRowContaining(companyName, table));
+
+            if (countryChkBox.Checked == false)
+                Mouse.Click(countryChkBox);
+
+        }
 
 
     }
